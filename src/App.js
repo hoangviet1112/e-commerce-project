f388def4c137a79a5ab883b809f5ae635f8228c0
hoangviet1112 <nguyenduchoangviet93@gmail.com>
2021-10-23 20:32:12 +0700

    finish header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Switch, Route } from 'react-router';
 import './App.css';
+import Header from './components/header/header';
 import { Homepage } from './pages/homepage/Homepage';
 import { Shoppage } from './pages/shoppage/Shoppage';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path component={Homepage} />
         <Route path="/shop" component={Shoppage} />
