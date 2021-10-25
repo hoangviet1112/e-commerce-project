c79e6f4872dfd82565d09a5181d8bd672ff19e92
unknown <nguyenduchoangviet93@gmail.com>
2021-10-25 22:34:24 +0700

    Bypass Firebase Module

--- src/App.js
+++ src/App.js
@@ -4,19 +4,8 @@ import Header from './components/header/header';
 import { Homepage } from './pages/homepage/Homepage';
 import { Shoppage } from './pages/shoppage/Shoppage';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/SignInAndSignUpPage';
-import {auth} from "./firebase/firebase.utils"
-import { useState, useEffect } from 'react';
 
 function App() {
-  
-  const [currentUser, setCurrentUser] = useState()
-  
-  useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      setCurrentUser({ currentUser: user})
-      console.log(user);
-    })
-  })
   return (
     <div>
       <Header />
