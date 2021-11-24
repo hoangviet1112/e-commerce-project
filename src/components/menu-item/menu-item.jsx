4e9b80a4c428e9aa3bea7399d4cfd58a4d68c4ce
unknown <nguyenduchoangviet93@gmail.com>
2021-11-24 11:36:25 +0700

    24/11

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import "../menu-item/menu-item.scss"
 
 export const MenuItem = (props) => {
+    function handleClick() {
+        console.log(props.title);
+    }
+    
     return (
         <div className={`${props.size} menu-item`}>
             <div style={{backgroundImage: `url(${props.imageUrl})`}} className="background-image"></div>
-            <div className="content">
+            <div onClick={handleClick} className="content">
                 <h1 className="title">{props.title}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>    
